24b4d5ae821cfe83f268dd9517cf000dc14f3187
Harsh Kumar <your-email@gmail.com>
2026-05-03 00:04:18 +0530

    fix: index.js duplicate react import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -19,14 +14,4 @@ root.render(
   </React.StrictMode>
 )
 
-// PWA Service Worker register karo
 serviceWorkerRegistration.register()
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
-)
